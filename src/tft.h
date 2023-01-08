f03d11ae85a5908399643d05cdfd07979f907616
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-09 06:35:53 +0700

    Update tft.h

--- src/tft.h
+++ src/tft.h
@@ -4,11 +4,6 @@
 #include <TFT_22_ILI9225.h>
 #include "rtc.h"
 
-
-//int16_t hr = 0, min = 0, sec = 0, day = 0, mth = 0, yr = 0, dow = 0;
-
-//char days_of_week[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
 void tft_init(TFT_22_ILI9225 &tft);
 void tft_print(TFT_22_ILI9225 &tft, int8_t percent, int16_t hr, int16_t min, int16_t sec, int16_t day, int16_t mth, int16_t yr, int16_t dow);
 #endif
