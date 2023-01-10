c42c4694fd5981d71147ab847e2432d0c092a604
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-10 10:15:34 +0700

    Update rtc.cpp

--- src/rtc.cpp
+++ src/rtc.cpp
@@ -3,7 +3,7 @@
 void rtc_init(uRTCLib &rtc, int16_t hr, int16_t min, int16_t sec, int16_t day, int16_t mth, int16_t yr, int16_t dow) 
 {
     URTCLIB_WIRE.begin();                                     /* set up RTC */
-    rtc.set(sec, min, hr, dow, day, mth, yr);                 /* set up time (second, minute, hour, dayOfWeek, dayOfMonth, month, year)*/
+    rtc.set(sec, min, hr, dow, day, mth, yr);                 /* set up time (second, minute, hour, dayOfWeek, dayOfMonth, month, year) */
 }
 
 void rtc_read(uRTCLib &rtc, int16_t &hr, int16_t &min, int16_t &sec, int16_t &day, int16_t &mth, int16_t &yr, int16_t &dow) 
