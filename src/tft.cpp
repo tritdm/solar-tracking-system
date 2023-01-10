268f4f9087d065353f1d71c121b0b993c20f2d13
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-10 10:14:56 +0700

    Update tft.cpp

--- src/tft.cpp
+++ src/tft.cpp
@@ -1,27 +1,25 @@
 #include "tft.h"
 
-void tft_init(TFT_22_ILI9225 &tft) {
-    // set up TFT
-  tft.begin();
-  tft.setOrientation(1);      // right rotated landscape
-  tft.setFont(Terminal6x8);   // set font wide 6px and high 8px
+void tft_init(TFT_22_ILI9225 &tft) 
+{
+    tft.begin();
+    tft.setOrientation(1);                                                          /* right rotated landscape */
+    tft.setFont(Terminal6x8);                                                       /* set font wide 6px and high 8px */
 }
 
-// print infomation to tft lcd function
-void tft_print(TFT_22_ILI9225 &tft, int8_t percent, int16_t hr, int16_t min, int16_t sec, int16_t day, int16_t mth, int16_t yr, int16_t dow) {
-  // thuc vao 10, thuc xuong 20
-  
-  char days_of_week[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-  
-  String time1 = String(hr) + ':' + String(min) + ':' + String(sec);
-  String time2 = String(days_of_week[dow]) + ", " + String(day) + '/' + String(mth) + '/' + String(2000 + yr);
-  String time3 = time1 + ' ' + time2;
-  String pc = String(percent) + "%";
+void tft_print(TFT_22_ILI9225 &tft, int8_t percent, int16_t hr, int16_t min, int16_t sec, int16_t day, int16_t mth, int16_t yr, int16_t dow) 
+{
+    char days_of_week[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
-  tft.drawText(98, 150, "Time");
-  tft.drawText(20, 140, time3);
-  tft.drawText(68, 130, "Battery percent");
-  tft.fillRectangle(60, 120, 160, 110, COLOR_WHITE);
-  tft.fillRectangle(60, 120, 60 + percent, 110, COLOR_BLUE);
-  tft.drawText(101, 100, pc);
+    String time1 = String(hr) + ':' + String(min) + ':' + String(sec);
+    String time2 = String(days_of_week[dow]) + ", " + String(day) + '/' + String(mth) + '/' + String(2000 + yr);
+    String time3 = time1 + ' ' + time2;
+    String pc = String(percent) + "%";
+
+    tft.drawText(98, 150, "Time");
+    tft.drawText(20, 140, time3);
+    tft.drawText(68, 130, "Battery percent");
+    tft.fillRectangle(60, 120, 160, 110, COLOR_WHITE);
+    tft.fillRectangle(60, 120, 60 + percent, 110, COLOR_BLUE);
+    tft.drawText(101, 100, pc);
 }
