6643bbb90998241cd0bb0818a56754358088c9ba
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-10 10:11:50 +0700

    Update sensors.cpp

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -1,10 +1,10 @@
 #include "sensors.h"
 
 void sensor_read(int16_t &lightE, int16_t &lightW, int16_t &lightS, int16_t &lightN, 
-                const int8_t p_resE, const int8_t p_resW, const int8_t p_resS, const int8_t p_resN) {
-    // read light intensity values
-  lightE = analogRead(p_resE);
-  lightW = analogRead(p_resW);
-  lightS = analogRead(p_resS);
-  lightN = analogRead(p_resN);
+                const int8_t p_resE, const int8_t p_resW, const int8_t p_resS, const int8_t p_resN) 
+{
+    lightE = analogRead(p_resE);
+    lightW = analogRead(p_resW);
+    lightS = analogRead(p_resS);
+    lightN = analogRead(p_resN);
 }
