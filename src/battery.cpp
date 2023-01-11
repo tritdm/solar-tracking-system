b1d1ad5cdaefe7d18d5e34a3a29822c732158153
Tạ Điền Minh Trí <91335704+tritdm@users.noreply.github.com>
2023-01-11 10:27:56 +0700

    Update battery.cpp

--- src/battery.cpp
+++ src/battery.cpp
@@ -4,11 +4,10 @@ void battert_calculator(int16_t &bav, float &vout, float &vcc, int8_t &percent)
 {
     bav = analogRead(A0);
     vout = bav * 5.0 / 1024;
-    // Thevenin - Norton chia áp R1 = 10k - R2 = 2k2
-    // vcc = vout * 22k2 / 2k2
-    vcc = vout * 12200.0 / 2200;
-    // calculator battery voltage
+    /* Thevenin - Norton chia áp R1 = 10k - R2 = 2k2 */
+    /* vcc = vout * 12k2 / 2k2 */
+    vcc = vout * 12200.0 / 2200;                /* calculator battery voltage */
     percent = vcc / 3.8 * 100;
-    if (percent > 100) percent = 100;
-    // calculator battery percent
+    if (percent > 100) percent = 100;           /* calculator battery percent */
+
 }
